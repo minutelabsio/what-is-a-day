475cadf88f2a68f3cc2234a7727c6ea05a485fdf
Jasper <well.caffeinated@gmail.com>
2018-12-10 20:36:02 -0500

    deploy fixes

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:minutelabsio/what-is-a-day.git master:gh-pages
+git push -f https://github.com/minutelabsio/what-is-a-day.git master:gh-pages
 
 cd -
