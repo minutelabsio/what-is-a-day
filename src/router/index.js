33407537f8ef7a89c6d5386af67d7f62fde897b5
Jasper <well.caffeinated@gmail.com>
2019-04-18 17:27:06 -0400

    new assets

--- src/router/index.js
+++ src/router/index.js
@@ -10,38 +10,27 @@ export default new Router({
       path: '/'
       , name: 'player'
       , component: PlayerUI
-      , redirect: { name: 'stellar-v-solar' }
+      , redirect: { name: 'stellar' }
       , children: [
         {
-          path: 'stellar-v-solar'
-          , name: 'stellar-v-solar'
+          path: 'stellar'
+          , name: 'stellar'
           , component: () => import('@/components/chapters/sidereal-v-solar')
           , meta: {
-            title: 'Sidereal vs. Solar Days'
+            title: 'Stellar Days'
             , audio: [
-              require('@/assets/v1/Siderial vs Solar days.mp3')
+              require('@/assets/v2/ML11-stellar-days.mp3')
             ]
           }
         }
         , {
-          path: 'elliptic-orbit'
-          , name: 'elliptic-orbit'
+          path: 'solar'
+          , name: 'solar'
           , component: () => import('@/components/chapters/elliptic-orbit')
           , meta: {
-            title: 'The Earth\'s Elliptic Orbit'
+            title: 'Solar Days'
             , audio: [
-              require('@/assets/v1/Eccentricity.mp3')
-            ]
-          }
-        }
-        , {
-          path: 'axial-tilt'
-          , name: 'axial-tilt'
-          , component: () => import('@/components/chapters/axial-tilt')
-          , meta: {
-            title: 'Axial Tilt'
-            , audio: [
-              require('@/assets/v1/Axial Tilt.mp3')
+              require('@/assets/v2/ML11-solar-days.mp3')
             ]
           }
         }
