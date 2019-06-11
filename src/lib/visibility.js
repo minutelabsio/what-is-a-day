5b574c2b8e3ac51e7218154ee4de4dcd3cdbccc7
Jasper <well.caffeinated@gmail.com>
2019-06-11 12:23:48 -0600

    eslint

--- src/lib/visibility.js
+++ src/lib/visibility.js
@@ -1,9 +1,9 @@
 const hiddenProperty = 'hidden' in document ? 'hidden' :
   'webkitHidden' in document ? 'webkitHidden' :
   'mozHidden' in document ? 'mozHidden' : null
-const visibilityStateProperty = 'visibilityState' in document ? 'visibilityState' :
-  'webkitVisibilityState' in document ? 'webkitVisibilityState' :
-  'mozVisibilityState' in document ? 'mozVisibilityState' : null
+// const visibilityStateProperty = 'visibilityState' in document ? 'visibilityState' :
+//   'webkitVisibilityState' in document ? 'webkitVisibilityState' :
+//   'mozVisibilityState' in document ? 'mozVisibilityState' : null
 const visibilityChangeEvent = hiddenProperty && hiddenProperty.replace(/hidden/i, 'visibilitychange')
 const isSupported = !!hiddenProperty
 
